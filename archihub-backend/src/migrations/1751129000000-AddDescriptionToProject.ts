2b1d48d5deb59c4dcb17c2c9d82e61ea150a8e9b
gares92 <66022318+gares92@users.noreply.github.com>
2025-06-29 00:04:22 +0200

    fix(migration): ajout colonne name sur projects sans erreur NOT NULL, et description

--- archihub-backend/src/migrations/1751129000000-AddDescriptionToProject.ts
+++ archihub-backend/src/migrations/1751129000000-AddDescriptionToProject.ts
@@ -5,9 +5,13 @@ export class AddDescriptionToProject1751129000000 implements MigrationInterface 
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "projects" ADD "description" character varying`);
+        await queryRunner.query(`ALTER TABLE "projects" ADD "name" character varying`);
+        await queryRunner.query(`UPDATE "projects" SET "name" = 'Projet sans nom' WHERE "name" IS NULL`);
+        await queryRunner.query(`ALTER TABLE "projects" ALTER COLUMN "name" SET NOT NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "projects" DROP COLUMN "name"`);
         await queryRunner.query(`ALTER TABLE "projects" DROP COLUMN "description"`);
     }
 } 
