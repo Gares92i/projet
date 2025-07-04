2f426be14cdf462ba9482416e047045d29056730
gares92 <66022318+gares92@users.noreply.github.com>
2025-07-04 21:27:09 +0200

    Fix: Correction de l'import multer dans uploads.module.ts pour résoudre l'erreur memoryStorage

--- archihub-backend/src/uploads/uploads.module.ts
+++ archihub-backend/src/uploads/uploads.module.ts
@@ -3,8 +3,9 @@ import { MulterModule } from '@nestjs/platform-express';
 import { UploadsController } from './uploads.controller';
 import { UploadsService } from './uploads.service';
 import { ConfigModule } from '@nestjs/config';
-import multer from 'multer';
 import { UploadThingService } from './uploadthing.service';
+
+const multer = require('multer');
 
 @Module({
   imports: [
